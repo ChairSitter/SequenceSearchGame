f86c3e0faf625272d5826c6f8e0421cad043640d
Bryan Schneller <bryan.schneller@gmail.com>
2024-02-01 15:32:51 -0500

    Change borders to the same color as the inside of each circle; make initial circles lgray with lgray borders

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-let initial = true;
 const numberShadeDivs = () => {
+    console.log("ran")
     for(let i = 0; i < 10; i++){
         for(let j = 0; j < 10; j++){
             let circle = document.querySelector(`#box-${i+1}-${j+1}`);
             let randomNumber = Math.floor(Math.random()*10);
+            let circleColor = `rgb(${(9 - randomNumber)*28}, ${(9 - randomNumber)*28}, ${(9 - randomNumber)*28})`;
             circle.textContent = randomNumber;
-            circle.style.backgroundColor = `rgb(${(9 - randomNumber)*28}, ${(9 - randomNumber)*28}, ${(9 - randomNumber)*28})`; 
-            circle.style.fontSize = `${1+(randomNumber * .25)}rem`;
+            circle.style.backgroundColor = circleColor; 
+            circle.style.fontSize = `${1 + (randomNumber * .25)}rem`;
             circle.style.color = "black";
-            circle.style.border = "5px solid black";
+            circle.style.border = `5px solid ${circleColor}`;
             if(randomNumber > 4){
                 circle.style.color = "white";
             }
-            if(initial === true){
-                circle.addEventListener("click", function(){
-                if(circle.style.border == "5px solid black"){
-                    circle.style.border = "5px solid red";
-                } else if (circle.style.border == "5px solid red"){
-                    circle.style.border = "5px solid black";
+            let isFirst = false;
+            circle.addEventListener("click", function(){
+                if (isFirst){
+                    circle.style.border = `5px solid ${circleColor}`;
+                } else {
+                    circle.style.border = `5px solid red`;
                 }
-                initial = false;
+                isFirst = !isFirst;
             })
-            }
         }
     }
 }
